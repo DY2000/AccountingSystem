b56819d66bbbb2f5315c780c31a7d9e242d7fda0
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-03-13 15:16:21 -0400

    TIGRE

--- OrcMathGUI/src/accounting/MainScreen.java
+++ OrcMathGUI/src/accounting/MainScreen.java
@@ -1,10 +1,12 @@
 package accounting;
 
 import java.awt.Color;
-
+import java.io.File;
 import java.util.List;
 
-import guiPlayer.Sampler;
+import javax.swing.JFrame;
+
+
 import guiTeacher.components.Action;
 import guiTeacher.components.Button;
 import guiTeacher.components.Checkbox;
@@ -14,7 +16,7 @@ import guiTeacher.interfaces.FileRequester;
 import guiTeacher.interfaces.Visible;
 import guiTeacher.userInterfaces.FullFunctionScreen;
 
-public class MainScreen extends FullFunctionScreen {
+public class MainScreen extends FullFunctionScreen implements FileRequester {
 
 	public MainScreen(int width, int height) {
 		super(width, height);
@@ -36,8 +38,8 @@ public class MainScreen extends FullFunctionScreen {
 				if(visible) {
 						int i = a;
 						a = i;
-						TextBox box = new TextBox(20 + 50 * i, 160, 200, 100, "Try typing here.");
-						box.setVisible(visible);
+						TextBox box = new TextBox(20 + 100 * i, 160, 200, 100, "Try typing here.");
+						//box.setVisible(visible);
 						viewObjects.add(box);
 						visible = false;
 				}
@@ -50,8 +52,22 @@ public class MainScreen extends FullFunctionScreen {
 			}
 		}); 
 		viewObjects.add(check);
-		FileOpenButton fileButton = new FileOpenButton(490, 70, 120, 30, null, MainScreen.this);
+		FileOpenButton fileButton = new FileOpenButton(100, 70, 120, 30, null, MainScreen.this);
 		viewObjects.add(fileButton);
+		TextBox box1 = new TextBox(20, 210, 200, 100, "wow");
+		viewObjects.add(box1);
+	}
+
+	@Override
+	public void setFile(File f) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public JFrame getWindow() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 	
 }
