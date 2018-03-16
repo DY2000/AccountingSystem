04083e9549fc8e4cf19e97af8a94ff3ae9f3621a
BT_1N3_33 <BT_1N3_33@BT_1N3_33.bktech.local>
2018-03-16 15:29:28 -0400

    progress

--- OrcMathGUI/src/accounting/MainScreen.java
+++ OrcMathGUI/src/accounting/MainScreen.java
@@ -3,6 +3,7 @@ package accounting;
 import java.awt.Color;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
@@ -19,6 +20,10 @@ import guiTeacher.userInterfaces.FullFunctionScreen;
 
 public class MainScreen extends FullFunctionScreen implements FileRequester {
 
+	private ArrayList<TextBox> boxes; //ap exam
+	private int a = 1;
+
+	
 	public MainScreen(int width, int height) {
 		super(width, height);
 		// TODO Auto-generated constructor stub
@@ -27,42 +32,59 @@ public class MainScreen extends FullFunctionScreen implements FileRequester {
 	@Override
 	public void initAllObjects(List<Visible> viewObjects) {
 
-		
+		boxes = new ArrayList<TextBox>(); 
 		Button check = new Button(250, 95, 200, 100, "Open", new Action() {
 			
 			boolean visible = true;
-			int a = 1;
+			
 			
 			public void act() {
 				
-				
-				//if(visible) {
+					
+
 						int i = a;
 						a = i;
-						TextBox box = new TextBox(20 + 100 * i, 160, 200, 100, "why u no work.");
-						
-						//box.setVisible(visible);
-						viewObjects.add(box);
-						
-						//visible = false;
+						TextBox box = new TextBox(20 + 100 * i, 160, 200, 100, "blank.");
+						addObject(box);
+						boxes.add(box);
 						a++;
 						
 					
-				//}
-				//if(!visible) {
-				//	a++;
-				//	visible = true;
 					
-				//}
-				
 				
 			}
 		}); 
+		Button delete = new Button(450, 95, 200, 100, "delete", new Action() {
+			
+
+			public void act() {
+				
+				if(boxes.size() > 0) {
+				TextBox lastBox = boxes.get(boxes.size()-1); // handles have to be lowercase
+				remove(lastBox);//removes from screen
+				boxes.remove(lastBox);//remove from reference
+				a--;
+				}
+				else {
+					
+				}
+					
+				
+			}
+		}); 
+		Button save = new Button(500,95,200,100, "save", new Action() {
+			
+			public void act() {
+				
+			}
+			
+		});
 		viewObjects.add(check);
+		viewObjects.add(delete);
 		FileOpenButton fileButton = new FileOpenButton(100, 70, 120, 30, null, MainScreen.this);
 		viewObjects.add(fileButton);
-		TextBox box1 = new TextBox(20, 210, 200, 100, "wow");
-		viewObjects.add(box1);
+		//TextBox box1 = new TextBox(20, 210, 200, 100, "wow");
+		//viewObjects.add(box1);
 	}
 
 	@Override
